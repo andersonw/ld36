0a0ea980181d9e52633ef7fd143c3fb425047ae6
Anderson Wang <anderson.wang1994@gmail.com>
2016-08-26 20:17:04 -0400

    hello world project

---
+++ game/source/MenuState.hx
@@ -0,0 +1,23 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+class MenuState extends FlxState
+{
+	override public function create():Void
+	{
+        add(new FlxText(10, 10, 100, "Hello, World!"));
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
