759cfad5547e27c3876a5d850175d5c2bad88932
Anderson Wang <anderson.wang1994@gmail.com>
2016-08-29 21:15:48 -0400

    small text fix

--- game/source/CreditsState.hx
+++ game/source/CreditsState.hx
@@ -12,7 +12,7 @@ class CreditsState extends FlxState
 
     override public function create():Void
     {
-        creditsText = new FlxText((FlxG.width-creditsText.width)/2, 100, 1000, 13);
+        creditsText = new FlxText(FlxG.width/2, 100, 1000, 13);
         creditsText.text = "Force of Wheel was made for Ludum Dare 36\n Programmed by Phillip Ai, Max Murin, and Anderson Wang\n Created with HaxeFlixel, sound effects made with bfxr";
         creditsText.setFormat(Registry.FONT_PATH, 13, FlxColor.WHITE, CENTER);
         creditsText.x = (FlxG.width-creditsText.width)/2;
